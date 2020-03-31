6196ae645b822c41a962c4dbf02f0de17735f216
Thiemen-Shapet <60937258+Thiemen-Shapet@users.noreply.github.com>
2020-03-31 16:37:08 +0200

    Push 1 server

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,16 +2,18 @@
 
 <main>
   <div class="row align-items-center">
+  
+  <div class="col-12 col-lg-6">
+      <img src="<?= $page->image()->url(); ?>" height="200px" class="img-fluid profiel" alt="<?= $page->image()->title(); ?>" />
+    </div>
     <div class="col-12 col-lg-6">
       <h1><?= $page->introductiontitle() ?></h1>
       <p class="lead"><?= $page->introductiontext() ?></p>
       <a href="<?= $site->page('projects') ?>" class="btn btn-primary" role="button" aria-pressed="true">View my work</a>
       <a href="<?= $site->page('about') ?>" class="btn btn-outline-primary" role="button" aria-pressed="true">More about me</a>
-    </div>
-    <div class="col-12 col-lg-6">
-      <img src="<?= $page->image()->url(); ?>" class="img-fluid" alt="<?= $page->image()->title(); ?>" />
-    </div>
+    </div>  
   </div>
+  
   <?php
   if ($projectsPage = page('projects')): ?>
   <div class="row border-top">
